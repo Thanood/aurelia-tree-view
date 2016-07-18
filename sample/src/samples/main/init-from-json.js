223c74d6ab1c9b1088143343b8fcd262da33f997
Daniel Bendel <daniel.bendel@gmail.com>
2016-07-18 12:45:18 +0200

    chore(sample): add init from json example

---
+++ sample/src/samples/main/init-from-json.js
@@ -0,0 +1,52 @@
+import {
+  NodeModel
+} from 'aurelia-tree-view';
+
+export class InitFromJson {
+  nodes = [];
+
+  attached() {
+    this.nodes = NodeModel.createFromJSON(this.getNodes());
+  }
+
+  getNodes() {
+    return [{
+      title: 'Texas',
+      children: [{
+        title: 'Austin'
+      }, {
+        title: 'Houston'
+      }]
+    }, {
+      title: 'New York',
+      children: [{
+        title: 'New York City',
+        children: [{
+          title: 'Manhattan'
+        }, {
+          title: 'Brooklyn'
+        }, {
+          title: 'Bronx'
+        }, {
+          title: 'Queens'
+        }, {
+          title: 'Staten Island'
+        }]
+      }, {
+        title: 'Buffalo'
+      }]
+    }, {
+      title: 'Oregon',
+      children: [{
+        title: 'Portland'
+      }]
+    }, {
+      title: 'California',
+      children: [{
+        title: 'Los Angeles'
+      }, {
+        title: 'San Francisco'
+      }]
+    }];
+  }
+}
