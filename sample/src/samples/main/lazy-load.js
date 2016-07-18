07105f97d5ec960f933b357c4c9ac9b02d0befdc
Daniel Bendel <daniel.bendel@gmail.com>
2016-07-18 12:35:07 +0200

    chore(sample): remove some comments

--- sample/src/samples/main/lazy-load.js
+++ sample/src/samples/main/lazy-load.js
@@ -5,40 +5,6 @@ export class LazyLoad {
   forceLazyLoad = false;
 
   attached() {
-    let texas = new NodeModel('Texas', [
-      new NodeModel('Austin'),
-      new NodeModel('Houston')
-    ]);
-
-    let newYork = new NodeModel('New York', [
-      new NodeModel('New York City', () => {
-        return new Promise((resolve, reject) => {
-          window.setTimeout(() => {
-            resolve([
-              new NodeModel('Manhattan'),
-              new NodeModel('Brooklyn'),
-              new NodeModel('Bronx'),
-              new NodeModel('Queens'),
-              new NodeModel('Staten Island')
-            ]);
-          }, 500);
-        });
-      }),
-      new NodeModel('Buffalo')]);
-
-    let oregon = new NodeModel('Oregon', [new NodeModel('Portland')]);
-
-    let california = new NodeModel('California', () => {
-      return new Promise((resolve, reject) => {
-        window.setTimeout(() => {
-          resolve([
-            new NodeModel('Los Angeles'),
-            new NodeModel('San Francisco')
-          ]);
-        }, 40);
-      });
-    });
-    // this.nodes = [texas, newYork, oregon, california];
     this.nodes = NodeModel.createFromJSON(this.getNodes());
   }
 
