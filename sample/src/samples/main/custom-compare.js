54b16008958bfd5a5fba6f5189d502bd6889ff33
Daniel Bendel <daniel.bendel@gmail.com>
2016-07-27 16:19:45 +0200

    chore(sample): show lazy loading in preselect example

--- sample/src/samples/main/custom-compare.js
+++ sample/src/samples/main/custom-compare.js
@@ -10,14 +10,20 @@ export class CustomCompare {
       new NodeModel('Houston')
     ]);
 
+    let manhattan = new NodeModel('Manhattan');
+    let brooklyn = new NodeModel('Brooklyn');
+    let bronx = new NodeModel('Bronx');
+    let queens = new NodeModel('Queens');
+    let statenIsland = new NodeModel('Staten Island');
+
     let newYork = new NodeModel('New York', [
-      new NodeModel('New York City', [
-        new NodeModel('Manhattan'),
-        new NodeModel('Brooklyn'),
-        new NodeModel('Bronx'),
-        new NodeModel('Queens'),
-        new NodeModel('Staten Island')
-      ]),
+      new NodeModel('New York City', () => {
+        return new Promise(resolve => {
+          window.setTimeout(() => {
+            resolve([manhattan, brooklyn, bronx, queens, statenIsland]);
+          }, 500);
+        });
+      }),
       new NodeModel('Buffalo')]);
 
     let oregon = new NodeModel('Oregon', [new NodeModel('Portland')]);
@@ -27,7 +33,7 @@ export class CustomCompare {
       new NodeModel('San Francisco')
     ]);
     this.nodes = [texas, newYork, oregon, california];
-    this.selected = [oregon, oregon.children[0]];
+    this.selected = [oregon, oregon.children[0], newYork, newYork.children[0], manhattan, brooklyn, bronx, queens, statenIsland];
   }
 
   compareNode(a, b) {
