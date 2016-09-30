6633addeef42fa2e7329322c9cf2cdb79afa1399
Daniel Bendel <daniel.bendel@gmail.com>
2016-09-30 15:51:10 +0200

    chore(all): add template linter

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 var eslint = require('gulp-eslint');
+var templateLinter = require('gulp-aurelia-template-lint');
+var templateLinterConfig = new (require('aurelia-template-lint').Config);
 
 gulp.task('lint', function() {
   return gulp.src([paths.source, paths.sampleSrc, paths.tests])
   .pipe(eslint())
   .pipe(eslint.format());
   // .pipe(eslint.failOnError());
+});
+
+gulp.task('template-lint', function() {
+  // templateLinterConfig.obsoleteTagOpts.push({tag:"ul"});
+  return gulp.src(paths.html)
+    .pipe(templateLinter(templateLinterConfig));
 });
