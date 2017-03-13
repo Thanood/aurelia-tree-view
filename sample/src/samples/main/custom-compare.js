ae711966e721a7ef6894ad00c316cbc6617f5845
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-13 16:09:53 +0100

    chore(sample): code format

--- sample/src/samples/main/custom-compare.js
+++ sample/src/samples/main/custom-compare.js
@@ -1,72 +1,72 @@
 export class CustomCompare {
-    attached() {
-        this.tree.dataSource.load(this.getNodes());
-        this.tree.dataSource.selectNodes([
-            { id: 2 }, { id: 21 }, { id: 22 },
-            { id: 211, parent: { id: 21 } },
-            { id: 212, parent: { id: 21 } },
-            { id: 213, parent: { id: 21 } },
-            { id: 214, parent: { id: 21 } },
-            { id: 215, parent: { id: 21 } }
-        ]);
-    }
+  attached() {
+    this.tree.dataSource.load(this.getNodes());
+    this.tree.dataSource.selectNodes([
+      { id: 2 }, { id: 21 }, { id: 22 },
+      { id: 211, parent: { id: 21 } },
+      { id: 212, parent: { id: 21 } },
+      { id: 213, parent: { id: 21 } },
+      { id: 214, parent: { id: 21 } },
+      { id: 215, parent: { id: 21 } }
+    ]);
+  }
 
-    compareNode(a, b) {
-        return a.id === b.id;
-    }
+  compareNode(a, b) {
+    return a.id === b.id;
+  }
 
-    getNodes() {
-        return [
-            {
-                id: 1,
-                title: 'Texas',
-                children: [
-                    { id: 11, title: 'Austin' },
-                    { id: 12, title: 'Houston' }
-                ]
-            }, {
-                id: 2,
-                title: 'New York',
-                children: [
-                    {
-                        id: 21,
-                        title: 'New York City',
-                        children: () => {
-                            return new Promise((resolve, reject) => {
-                                window.setTimeout(function () {
-                                    resolve([
-                                        { id: 211, title: 'Manhattan' },
-                                        { id: 212, title: 'Brooklyn' },
-                                        { id: 213, title: 'Bronx' },
-                                        { id: 214, title: 'Queens' },
-                                        { id: 215, title: 'Staten Island' }
-                                    ]);
-                                }, 500);
-                            });
-                        }
-                    }, {
-                        id: 22,
-                        title: 'Buffalo'
-                    }
-                ]
-            }, {
-                id: 3,
-                title: 'Oregon',
-                children: [
-                    { id: 31, title: 'Portland' }
-                ]
-            }, {
-                id: 4,
-                title: 'California',
-                children: () => {
-                    return new Promise((resolve, reject) => {
-                        resolve([
-                            { id: 41, title: 'Los Angeles' },
-                            { id: 42, title: 'San Francisco' }
-                        ]);
-                    });
-                }
+  getNodes() {
+    return [
+      {
+        id: 1,
+        title: 'Texas',
+        children: [
+          { id: 11, title: 'Austin' },
+          { id: 12, title: 'Houston' }
+        ]
+      }, {
+        id: 2,
+        title: 'New York',
+        children: [
+          {
+            id: 21,
+            title: 'New York City',
+            children: () => {
+              return new Promise((resolve, reject) => {
+                window.setTimeout(function () {
+                  resolve([
+                    { id: 211, title: 'Manhattan' },
+                    { id: 212, title: 'Brooklyn' },
+                    { id: 213, title: 'Bronx' },
+                    { id: 214, title: 'Queens' },
+                    { id: 215, title: 'Staten Island' }
+                  ]);
+                }, 500);
+              });
             }
-        ];
-    }
+          }, {
+            id: 22,
+            title: 'Buffalo'
+          }
+        ]
+      }, {
+        id: 3,
+        title: 'Oregon',
+        children: [
+          { id: 31, title: 'Portland' }
+        ]
+      }, {
+        id: 4,
+        title: 'California',
+        children: () => {
+          return new Promise((resolve, reject) => {
+            resolve([
+              { id: 41, title: 'Los Angeles' },
+              { id: 42, title: 'San Francisco' }
+            ]);
+          });
+        }
+      }
+    ];
+  }
 }
