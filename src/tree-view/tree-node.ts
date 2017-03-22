42162cd05516e105fdab5c1e6c1a4143acade5df
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-22 16:00:16 +0100

    style(all): code format

--- src/tree-view/tree-node.ts
+++ src/tree-view/tree-node.ts
@@ -1,121 +1,122 @@
-import {computedFrom, createOverrideContext} from 'aurelia-binding';
-import {inject, Container} from 'aurelia-dependency-injection';
-import {getLogger, Logger} from 'aurelia-logging';
-import {DOM} from 'aurelia-pal';
-import {TaskQueue} from 'aurelia-task-queue';
-import {bindable, ViewCompiler, ViewResources, ViewSlot} from 'aurelia-templating';
-import {NodeModel} from './node-model';
-import {TemplateInfo} from './template-info';
+import { computedFrom, createOverrideContext } from 'aurelia-binding';
+import { inject, Container } from 'aurelia-dependency-injection';
+import { getLogger, Logger } from 'aurelia-logging';
+import { DOM } from 'aurelia-pal';
+import { TaskQueue } from 'aurelia-task-queue';
+import { bindable, ViewCompiler, ViewResources, ViewSlot } from 'aurelia-templating';
+import { NodeModel } from './node-model';
+import { TemplateInfo } from './template-info';
 
 @inject(Element, ViewCompiler, ViewResources, Container, TaskQueue)
 export class TreeNode {
-    log: Logger;
-    isSelected: boolean;
-    templateTarget: Node;
-    viewSlot: ViewSlot | null;
+  log: Logger;
+  isSelected: boolean;
+  templateTarget: Node;
+  viewSlot: ViewSlot | null;
 
-    @bindable() model: NodeModel;
+  @bindable() model: NodeModel;
 
-    constructor(private element: Element, private viewCompiler: ViewCompiler, private viewResources: ViewResources, private container: Container, private taskQueue: TaskQueue) {
-        this.isSelected = false;
-        this.log = getLogger('aurelia-tree-view/tree-node');
-        this.viewSlot = null;
+  constructor(private element: Element, private viewCompiler: ViewCompiler, private viewResources: ViewResources, private container: Container, private taskQueue: TaskQueue) {
+    this.isSelected = false;
+    this.log = getLogger('aurelia-tree-view/tree-node');
+    this.viewSlot = null;
+  }
+
+  attached() {
+    // this.log.debug('attached called');
+    this.updateTemplate();
+  }
+
+  @computedFrom('model.dataSourceApi.settings')
+  get hasTemplate() {
+    return !!(this.model.dataSourceApi.settings && this.model.dataSourceApi.settings.templateInfo);
+  }
+
+  modelChanged(newValue: NodeModel) {
+    if (newValue) {
+      newValue.element = this;
+      if (newValue.children) {
+        newValue.children.forEach(child => {
+          child.isVisible = false;
+        });
+      }
     }
+  }
 
-    attached() {
-        this.updateTemplate();
+  focus(e: Event, permitBubbles: boolean = false) {
+    this.model.dataSourceApi.focusNode(this.model);
+    return permitBubbles;
+  }
+
+  toggleExpanded(e: MouseEvent): Promise<void> {
+    // TODO: only change this using a store
+    let promise: Promise<void>;
+    if (this.model.isExpanded) {
+      promise = this.model.collapse().then(() => {
+        const event = DOM.createCustomEvent('collapsed', { bubbles: true, detail: { node: this.model } });
+        this.element.dispatchEvent(event);
+      });
+    } else {
+      promise = this.model.expand().then(() => {
+        const event = DOM.createCustomEvent('expanded', { bubbles: true, detail: { node: this.model } });
+        this.element.dispatchEvent(event);
+      });
     }
-
-    @computedFrom('model.dataSourceApi.settings')
-    get hasTemplate() {
-        return !!(this.model.dataSourceApi.settings && this.model.dataSourceApi.settings.templateInfo);
+    const processChildren = (<any>e)[`${this.model.dataSourceApi.settings.processChildrenKey}Key`];
+    if (this.model && processChildren) {
+      promise = promise.then(() => {
+        this.model.dataSourceApi.expandNodeAndChildren(this.model);
+      });
     }
+    return promise;
+  }
 
-    modelChanged(newValue: NodeModel) {
-        if (newValue) {
-            newValue.element = this;
-            if (newValue.children) {
-                newValue.children.forEach(child => {
-                    child.isVisible = false;
-                });
-            }
-        }
-    }
-
-    focus(e: Event, permitBubbles: boolean = false) {
-        this.model.dataSourceApi.focusNode(this.model);
-        return permitBubbles;
-    }
-
-    toggleExpanded(e: MouseEvent): Promise<void> {
-        // TODO: only change this using a store
-        let promise: Promise<void>;
-        if (this.model.isExpanded) {
-            promise = this.model.collapse().then(() => {
-                const event = DOM.createCustomEvent('collapsed', { bubbles: true, detail: { node: this.model }});
-                this.element.dispatchEvent(event);
-            });
-        } else {
-            promise = this.model.expand().then(() => {
-                const event = DOM.createCustomEvent('expanded', { bubbles: true, detail: { node: this.model }});
-                this.element.dispatchEvent(event);
-            });
-        }
+  toggleSelected(e: MouseEvent, permitBubbles: boolean = false) {
+    if (this.model) {
+      this.taskQueue.queueTask(() => {
         const processChildren = (<any>e)[`${this.model.dataSourceApi.settings.processChildrenKey}Key`];
-        if (this.model && processChildren) {
-            promise = promise.then(() => {
-                this.model.dataSourceApi.expandNodeAndChildren(this.model);
-            });
+        const processChildrenRecursive = (<any>e)[`${this.model.dataSourceApi.settings.processChildrenRecursiveKey}Key`];
+        if (this.isSelected) {
+          this.model.dataSourceApi.selectNode(this.model, processChildren, processChildrenRecursive);
+        } else {
+          this.model.dataSourceApi.deselectNode(this.model, processChildren, processChildrenRecursive);
         }
-        return promise;
+      });
     }
+    return permitBubbles;
+  }
 
-    toggleSelected(e: MouseEvent, permitBubbles: boolean = false) {
-        if (this.model) {
-            this.taskQueue.queueTask(() => {
-                const processChildren = (<any>e)[`${this.model.dataSourceApi.settings.processChildrenKey}Key`];
-                const processChildrenRecursive = (<any>e)[`${this.model.dataSourceApi.settings.processChildrenRecursiveKey}Key`];
-                if (this.isSelected) {
-                    this.model.dataSourceApi.selectNode(this.model, processChildren, processChildrenRecursive);
-                } else {
-                    this.model.dataSourceApi.deselectNode(this.model, processChildren, processChildrenRecursive);
-                }
-            });
-        }
-        return permitBubbles;
+  unbindTemplate() {
+    if (this.viewSlot) {
+      // @vegarringdal said, switch detached and unbind
+      this.viewSlot.detached();
+      this.viewSlot.unbind();
+      this.viewSlot.removeAll();
     }
+  }
 
-    unbindTemplate() {
-        if (this.viewSlot) {
-            // @vegarringdal said, switch detached and unbind
-            this.viewSlot.detached();
-            this.viewSlot.unbind();
-            this.viewSlot.removeAll();
-        }
-    }
+  updateTemplate() {
+    this.unbindTemplate();
+    this.useTemplate();
+  }
 
-    updateTemplate() {
-        this.unbindTemplate();
-        this.useTemplate();
+  useTemplate() {
+    if (this.hasTemplate) {
+      if (this.model && this.model.dataSourceApi.settings && this.model.dataSourceApi.settings.templateInfo) {
+        const templateInfo: TemplateInfo = this.model.dataSourceApi.settings.templateInfo;
+        const viewFactory = this.viewCompiler.compile(`<template>${templateInfo.template}</template>`, this.viewResources);
+        const view = viewFactory.create(this.container);
+        this.viewSlot = new ViewSlot(this.templateTarget, true);
+        this.viewSlot.add(view);
+        this.viewSlot.bind(this, createOverrideContext(this, createOverrideContext(templateInfo.viewModel)));
+        this.viewSlot.attached();
+      } else {
+        this.log.warn('hasTemplate is true but something was not set');
+        this.log.warn('templateTarget', this.templateTarget);
+        this.log.warn('settings', this.model.dataSourceApi.settings);
+        this.log.warn('templateInfo', this.model.dataSourceApi.settings ? this.model.dataSourceApi.settings.templateInfo : 'null');
+        this.log.warn('model', this.model);
+      }
     }
-
-    useTemplate() {
-        if (this.hasTemplate) {
-            if (this.model && this.model.dataSourceApi.settings && this.model.dataSourceApi.settings.templateInfo) {
-                const templateInfo: TemplateInfo = this.model.dataSourceApi.settings.templateInfo;
-                const viewFactory = this.viewCompiler.compile(`<template>${templateInfo.template}</template>`, this.viewResources);
-                const view = viewFactory.create(this.container);
-                this.viewSlot = new ViewSlot(this.templateTarget, true);
-                this.viewSlot.add(view);
-                this.viewSlot.bind(this, createOverrideContext(this, createOverrideContext(templateInfo.viewModel)));
-                this.viewSlot.attached();
-            } else {
-                this.log.warn('hasTemplate is true but something was not set');
-                this.log.warn('templateTarget', this.templateTarget);
-                this.log.warn('settings', this.model.dataSourceApi.settings);
-                this.log.warn('templateInfo', this.model.dataSourceApi.settings ? this.model.dataSourceApi.settings.templateInfo : 'null');
-                this.log.warn('model', this.model);
-            }
-        }
-    }
+  }
 }
--- src/tree-view/tree-view.ts
+++ src/tree-view/tree-view.ts
@@ -1,80 +1,82 @@
-import {Disposable} from 'aurelia-binding';
-import {inject} from 'aurelia-dependency-injection';
-import {getLogger, Logger} from 'aurelia-logging';
-import {TaskQueue} from 'aurelia-task-queue';
-import {DOM} from 'aurelia-pal';
-import {bindable, child} from 'aurelia-templating';
-import {DataSource} from './data-source';
-import {NodeModel} from './node-model';
-import {TemplateInfo} from './template-info';
-import {TreeNodeTemplate} from './tree-node-template';
+import { Disposable } from 'aurelia-binding';
+import { inject } from 'aurelia-dependency-injection';
+import { getLogger, Logger } from 'aurelia-logging';
+import { TaskQueue } from 'aurelia-task-queue';
+import { DOM } from 'aurelia-pal';
+import { bindable, child } from 'aurelia-templating';
+import { DataSource } from './data-source';
+import { NodeModel } from './node-model';
+import { TemplateInfo } from './template-info';
+import { TreeNodeTemplate } from './tree-node-template';
 
 @inject(Element, TaskQueue)
 export class TreeView {
-    dataSource: DataSource;
-    private log: Logger;
-    private nodes: NodeModel[];
-    private subscriptions: Disposable[];
-    private templateInfo: TemplateInfo;
+  dataSource: DataSource;
+  private log: Logger;
+  private nodes: NodeModel[];
+  private subscriptions: Disposable[];
+  private templateInfo: TemplateInfo;
 
-    @bindable() compareEquality: ((args: { a: NodeModel, b: NodeModel }) => boolean);
-    @bindable() expandOnFocus: boolean = false;
-    @bindable() multiSelect: boolean = false;
-    @bindable() processChildrenKey = 'ctrl';
-    @bindable() processChildrenRecursiveKey = 'alt';
-    @child('tree-node-template') templateElement: TreeNodeTemplate;
+  @bindable() compareEquality: ((args: { a: NodeModel, b: NodeModel }) => boolean);
+  @bindable() expandOnFocus: boolean = false;
+  @bindable() multiSelect: boolean = false;
+  @bindable() processChildrenKey = 'ctrl';
+  @bindable() processChildrenRecursiveKey = 'alt';
+  @child('tree-node-template') templateElement: TreeNodeTemplate;
 
-    constructor(private element: Element, private taskQueue: TaskQueue) {
-        this.compareEquality = (args) => { return args.a === args.b; };
-        this.log = getLogger('aurelia-tree-view');
-        this.nodes = [];
-        this.subscriptions = [];
+  constructor(private element: Element, private taskQueue: TaskQueue) {
+    this.compareEquality = (args) => { return args.a === args.b; };
+    this.log = getLogger('aurelia-tree-view');
+    this.nodes = [];
+    this.subscriptions = [];
+  }
+
+  bind() {
+    this.multiSelect = ((this.multiSelect as any) === 'true' || this.multiSelect === true);
+    if (!this.dataSource) {
+      this.dataSource = new DataSource(this.taskQueue);
     }
+    // TODO: use a settings service or something similar
+    this.dataSource.settings.compareEquality = this.compareEquality;
+    this.dataSource.settings.expandOnFocus = this.expandOnFocus;
+    this.dataSource.settings.multiSelect = this.multiSelect;
+    this.dataSource.settings.processChildrenKey = this.processChildrenKey;
+    this.dataSource.settings.processChildrenRecursiveKey = this.processChildrenRecursiveKey;
+    this.subscriptions.push(this.dataSource.subscribe(this.handleDataSource.bind(this)));
+  }
 
-    bind() {
-        this.multiSelect = ((this.multiSelect as any) === 'true' || this.multiSelect === true);
-        if (!this.dataSource) {
-            this.dataSource = new DataSource(this.taskQueue);
+  unbind() {
+    // this.log.debug('disposing subscriptions:', this.subscriptions.length);
+    this.subscriptions.forEach(sub => sub.dispose());
+  }
+
+  attached() {
+    // this.taskQueue.queueTask(() => {
+      if (this.templateElement) {
+        const template = this.templateElement.template;
+        const viewModel = this.templateElement.model;
+        this.templateInfo = {
+          template,
+          viewModel
         }
-        // TODO: use a settings service or something similar
-        this.dataSource.settings.compareEquality = this.compareEquality;
-        this.dataSource.settings.expandOnFocus = this.expandOnFocus;
-        this.dataSource.settings.multiSelect = this.multiSelect;
-        this.dataSource.settings.processChildrenKey = this.processChildrenKey;
-        this.dataSource.settings.processChildrenRecursiveKey = this.processChildrenRecursiveKey;
-        this.subscriptions.push(this.dataSource.subscribe(this.handleDataSource.bind(this)));
-    }
+        this.dataSource.settings.templateInfo = this.templateInfo;
+        this.log.debug('template element found - template info:', this.templateInfo);
+      } else {
+        this.log.debug('no template element');
+      }
+    // });
+  }
 
-    unbind() {
-        // this.log.debug('disposing subscriptions:', this.subscriptions.length);
-        this.subscriptions.forEach(sub => sub.dispose());
+  handleDataSource(event: string, nodes: NodeModel[]) {
+    this.log.debug('data source', event, nodes);
+    switch (event) {
+      case 'loaded':
+        this.nodes = nodes;
+        break;
+      case 'selectionChanged':
+        const event = DOM.createCustomEvent('selection-changed', { bubbles: true, detail: { nodes } });
+        this.element.dispatchEvent(event);
+        break;
     }
-
-    attached() {
-        if (this.templateElement) {
-            const template = this.templateElement.template;
-            const viewModel = this.templateElement.model;
-            this.templateInfo = {
-                template,
-                viewModel
-            }
-            this.dataSource.settings.templateInfo = this.templateInfo;
-            this.log.debug('template element found template info', this.templateInfo);
-        } else {
-            this.log.debug('no template element');
-        }
-    }
-
-    handleDataSource(event: string, nodes: NodeModel[]) {
-        this.log.debug('data source', event, nodes);
-        switch (event) {
-            case 'loaded':
-                this.nodes = nodes;
-                break;
-            case 'selectionChanged':
-                const event = DOM.createCustomEvent('selection-changed', { bubbles: true, detail: {nodes} });
-                this.element.dispatchEvent(event);
-                break;
-        }
-    }
+  }
 }
