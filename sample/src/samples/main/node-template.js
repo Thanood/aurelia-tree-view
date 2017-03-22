fa400c903c081e340dfe0f552cab607d3ef0b8c7
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-22 15:19:14 +0100

    style(node-template): format code

--- sample/src/samples/main/node-template.js
+++ sample/src/samples/main/node-template.js
@@ -1,52 +1,52 @@
 export class NodeTemplate {
-    clickedNode = null;
-    nodeInterface = {
-        nodeClicked: (node) => {
-            console.log("I'm here", node);
-            this.clickedNode = node;
-        }
-    };
-
-    attached() {
-        this.tree.dataSource.load(this.getNodes());
+  clickedNode = null;
+  nodeInterface = {
+    nodeClicked: (node) => {
+      console.log("I'm here", node);
+      this.clickedNode = node;
     }
+  };
 
-    getNodes() {
-        return [
-            {
-                title: 'Texas',
-                children: [
-                    { title: 'Austin' },
-                    { title: 'Houston' }
-                ]
-            }, {
-                title: 'New York',
-                children: [
-                    {
-                        title: 'New York City',
-                        children: [
-                            { title: 'Manhattan' },
-                            { title: 'Brooklyn' },
-                            { title: 'Bronx' },
-                            { title: 'Queens' },
-                            { title: 'Staten Island' }
-                        ]
-                    }, {
-                        title: 'Buffalo'
-                    }
-                ]
-            }, {
-                title: 'Oregon',
-                children: [
-                    { title: 'Portland' }
-                ]
-            }, {
-                title: 'California',
-                children: [
-                    { title: 'Los Angeles' },
-                    { title: 'San Francisco' }
-                ]
-            }
-        ];
-    }
+  attached() {
+    this.tree.dataSource.load(this.getNodes());
+  }
+
+  getNodes() {
+    return [
+      {
+        title: 'Texas',
+        children: [
+          { title: 'Austin' },
+          { title: 'Houston' }
+        ]
+      }, {
+        title: 'New York',
+        children: [
+          {
+            title: 'New York City',
+            children: [
+              { title: 'Manhattan' },
+              { title: 'Brooklyn' },
+              { title: 'Bronx' },
+              { title: 'Queens' },
+              { title: 'Staten Island' }
+            ]
+          }, {
+            title: 'Buffalo'
+          }
+        ]
+      }, {
+        title: 'Oregon',
+        children: [
+          { title: 'Portland' }
+        ]
+      }, {
+        title: 'California',
+        children: [
+          { title: 'Los Angeles' },
+          { title: 'San Francisco' }
+        ]
+      }
+    ];
+  }
 }
