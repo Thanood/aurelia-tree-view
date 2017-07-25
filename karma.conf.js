4bdff52db21cf02bf1e62ba930bf03b43c60a677
Daniel Bendel <daniel.bendel@gmail.com>
2017-07-25 15:01:46 +0200

    chore(all): package cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -1,35 +1,23 @@
 module.exports = function(config) {
   config.set({
-
     basePath: '',
     frameworks: ['jasmine', 'requirejs'],
-
     files: [
-        'dist/test/test/main.js',
-        { pattern: 'dist/test/**/*.js', included: false, watched: true },
-        { pattern: 'node_modules/**/*.js', included: false, watched: false },
+      'dist/test/test/main.js',
+      { pattern: 'dist/test/**/*.js', included: false, watched: true },
+      { pattern: 'node_modules/**/*.js', included: false, watched: false },
     ],
-
     exclude: [
     ],
-
     preprocessors: {
     },
-
     reporters: ['progress'],
-
     port: 9876,
-
     colors: true,
-
     logLevel: config.LOG_INFO,
-
     autoWatch: true,
-
     browsers: ['Chrome'],
-
-    singleRun: false,
-
-    concurrency: Infinity
-  })
-}
+    singleRun: false
+    // concurrency: Infinity
+  });
+};
--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,5 +1,45 @@
+import {StageComponent} from 'aurelia-testing';
+import {bootstrap} from 'aurelia-bootstrapper';
+
 describe('index', () => {
     it('should work', () => {
-        expect(true)
-    })
-})
+        expect(true);
+    });
+});
+
+describe('tree-view', () => {
+  let component: any;
+
+  // beforeEach(() => {
+  //   component = StageComponent
+  //     .withResources('tree-view')
+  //     .inView(`<tree-view></tree-view>`)
+  //     .boundTo({});
+  // });
+
+  it('should render', () => {
+    component = StageComponent
+      .withResources('tree-view')
+      .inView(`<tree-view view-model.ref="tree"></tree-view>`)
+      .boundTo({
+        attached: () => {
+          this.tree.load([
+            { id: 0, title: 'test 1' },
+            { id: 1, title: 'test 2' }
+          ]);
+        }
+      });
+
+    component.create(bootstrap).then(() => {
+      const treeView = document.querySelector('tree-view');
+      
+      expect(treeView).not.toBeNull();
+      component.dispose();
+    });
+  });
+
+  // raises "Error: Cannot call ComponentTester.dispose() before ComponentTester.create()"
+  // afterEach(() => {
+  //   component.dispose();
+  // });
+});
