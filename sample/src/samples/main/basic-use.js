99cea1e87ff809975ed8b230a25a5eb800950a13
Daniel Bendel <daniel.bendel@gmail.com>
2017-07-25 15:20:19 +0200

    chore(sample): format code

--- sample/src/samples/main/basic-use.js
+++ sample/src/samples/main/basic-use.js
@@ -1,49 +1,49 @@
 export class BasicUse {
-    attached() {
-        const nodes = this.getNodes();
-        this.tree.dataSource.load(nodes);
-    }
+  attached() {
+    const nodes = this.getNodes();
+    this.tree.dataSource.load(nodes);
+  }
 
-    clearSelection() {
-        // this.tree.clearSelection();
-    }
+  clearSelection() {
+    // this.tree.clearSelection();
+  }
 
-    getNodes() {
-        return [
-            {
-                title: 'Texas',
-                children: [
-                    { title: 'Austin' },
-                    { title: 'Houston' }
-                ]
-            }, {
-                title: 'New York',
-                children: [
-                    {
-                        title: 'New York City',
-                        children: [
-                            { title: 'Manhattan' },
-                            { title: 'Brooklyn' },
-                            { title: 'Bronx' },
-                            { title: 'Queens' },
-                            { title: 'Staten Island' }
-                        ]
-                    }, {
-                        title: 'Buffalo'
-                    }
-                ]
-            }, {
-                title: 'Oregon',
-                children: [
-                    { title: 'Portland' }
-                ]
-            }, {
-                title: 'California',
-                children: [
-                    { title: 'Los Angeles' },
-                    { title: 'San Francisco' }
-                ]
-            }
-        ];
-    }
+  getNodes() {
+    return [
+      {
+        title: 'Texas',
+        children: [
+          { title: 'Austin' },
+          { title: 'Houston' }
+        ]
+      }, {
+        title: 'New York',
+        children: [
+          {
+            title: 'New York City',
+            children: [
+              { title: 'Manhattan' },
+              { title: 'Brooklyn' },
+              { title: 'Bronx' },
+              { title: 'Queens' },
+              { title: 'Staten Island' }
+            ]
+          }, {
+            title: 'Buffalo'
+          }
+        ]
+      }, {
+        title: 'Oregon',
+        children: [
+          { title: 'Portland' }
+        ]
+      }, {
+        title: 'California',
+        children: [
+          { title: 'Los Angeles' },
+          { title: 'San Francisco' }
+        ]
+      }
+    ];
+  }
 }
